27e9aac07f474479cc49508672f5711de2207898
Daniel Rios <danielrafaelrios@gmail.com>
2017-09-12 22:25:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(board)
-  puts "   |   |   "
+board = [" ", " ", " "," ", " ", " ", " ", " ", " "]
+
+def display_board(move = board)
+  puts " #{move[0]} | #{move[1]} | #{move[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{move[3]} | #{move[4]} | #{move[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{move[6]} | #{move[7]} | #{move[8]} "
 end
+
+
